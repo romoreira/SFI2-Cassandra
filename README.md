ac90b716295a468b317b36ad1b3298b7cc45bcac
romoreira <rodrigo.moreira206@gmail.com>
2023-06-07 15:34:21 +0000

    Doing on Azure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SFI2 Cassandra
+
+Installing Istio
+
+1. `$ curl -L https://istio.io/downloadIstio | sh -`
+2. `cd istio-1.17.3`
+3. `istioctl install --set profile=demo -y`
+4. `kubectl label namespace default istio-injection=enabled`
--- cassandra-depooyment/clean.sh
+++ cassandra-depooyment/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 grace=$(kubectl get pod cassandra-0 -o=jsonpath='{.spec.terminationGracePeriodSeconds}') \
-  && kubectl delete statefulset -l app=cassandra \
-  && echo "Sleeping ${grace} seconds" 1>&2 \
-  && sleep $grace \
+  && kubectl delete statefulset cassandra \
+  && echo "Sleeping 5 seconds" 1>&2 \
+  && sleep 5 \
   && kubectl delete persistentvolumeclaim -l app=cassandra
